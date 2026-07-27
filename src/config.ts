5ba596e34ca6cb1d49ed55193f8dc5ed7003d0dd
aitorjs <aitiba@gmail.com>
2026-07-27 14:51:20 +0200

    some meta little changes

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Diario de un hortelano | aitor ibañez';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'Constituye mi diario personal como hortelano y jardinero certificado, enfocado en el cultivo y la naturaleza. La página ofrece un blog de experiencias de huerto, un currículum profesional y una galería de imágenes.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
