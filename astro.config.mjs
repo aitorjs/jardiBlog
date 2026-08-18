130e34d2f36ba601857bd175f6f46d20c1e294f4
aitorjs <aitiba@gmail.com>
2026-08-18 14:22:33 +0200

    mini changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aitor.info',
-    base: '/',
-    integrations: [mdx(), sitemap()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: 'https://aitor.info',
+  base: '/',
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
